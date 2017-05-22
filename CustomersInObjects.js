727f91634bec13a7796adcc136f97e17a005e520
Greg <gpdoud@gmail.com>
2017-05-22 15:08:28 -0400

    added jqueryInputForm.html and CustomersInObjects.js

---
+++ CustomersInObjects.js
@@ -0,0 +1,29 @@
+var customers = [
+	{ 
+		id: 1000, 
+		name: "Waldorf Electonics", 
+		address: "123 Any Street",
+		city: "Berlin",
+		state: "Germany",
+		postalcode: "ABC123",
+		creditlimit: 100000 
+	}, 
+	{
+		id: 1010, 
+		name: "Google", 
+		address: "1 Google Way",
+		city: "Mountain View",
+		state: "California",
+		postalcode: "12345",
+		creditlimit: 1000000 
+	}, 
+	{
+		id: 1020, 
+		name: "Native Instruments", 
+		address: "2 Guitar Way",
+		city: "New York",
+		state: "New York",
+		postalcode: "10101", 
+		creditlimit: 10000 
+	}
+];
